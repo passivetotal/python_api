34c2dc4dcf7686f8bdc0705618f49d7e310a16a3
passivetotal <brandon@passivetotal.org>
2016-02-08 10:50:31 +0000

    Bumped versions

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='passivetotal',
-    version='1.0.10',
+    version='1.0.11',
     description='Client for the PassiveTotal REST API',
     url="https://github.com/passivetotal/python_api",
     author="Research Team, passivetotal",
