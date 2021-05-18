8d02a7a270792e0a18cc1e99ff607ef5bb309b93
Mark Kendrick <mark.kendrick@riskiq.com>
2021-05-18 09:57:52 -0700

    IP Whois as a distinct Analyzer object to complete as_dict availability.

--- passivetotal/analyzer/ip.py
+++ passivetotal/analyzer/ip.py
@@ -3,6 +3,7 @@
 
 from passivetotal.analyzer import get_api, get_config
 from passivetotal.analyzer._common import is_ip, AnalyzerError
+from passivetotal.analyzer.whois import IPWhois
 from passivetotal.analyzer.pdns import HasResolutions
 from passivetotal.analyzer.services import Services
 from passivetotal.analyzer.ssl import Certificates
@@ -101,7 +102,8 @@ class IPAddress(HasComponents, HasCookies, HasHostpairs, HasTrackers,
 
     def _api_get_whois(self):
         """Query the pDNS API for resolution history."""
-        self._whois = get_api('Whois').get_whois_details(query=self._ip)
+        response = get_api('Whois').get_whois_details(query=self._ip)
+        self._whois = IPWhois(response)
         return self._whois
     
     @property
@@ -124,9 +126,13 @@ class IPAddress(HasComponents, HasCookies, HasHostpairs, HasTrackers,
             return self._services
         return self._api_get_services()
     
+    
     @property
     def whois(self):
-        """Whois record details for this IP."""
+        """Most recently available Whois record for IP.
+
+        :rtype: :class:`passivetotal.analyzer.whois.IPWhois`
+        """
         if getattr(self, '_whois', None) is not None:
             return self._whois
         return self._api_get_whois()
